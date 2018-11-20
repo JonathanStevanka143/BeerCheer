de816ede270dacb78e7c162d0d4dd0456dcc8b27
Utsav Dave <utsavdipenbhai.dave01@stclairconnect.ca>
2018-11-20 01:30:59 -0500

    Added Constraint Layout to BeerColor Calculator Screen When the app switches to landscape mode, state is maintained Added Constraints on activity_main.xml

--- app/src/main/java/com/example/utsav/beercheer/BeerColorFragment.java
+++ app/src/main/java/com/example/utsav/beercheer/BeerColorFragment.java
@@ -3,6 +3,8 @@ package com.example.utsav.beercheer;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -84,6 +86,7 @@ public class BeerColorFragment extends Fragment {
                     + " must implement OnFragmentInteractionListener");
         }
     }
+
 
     @Override
     public void onDetach() {
